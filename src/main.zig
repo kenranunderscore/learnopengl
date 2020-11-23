3d17ebe2a5fa6d366b77894899a76429f1c29c01
Johannes Maier <johb.maier@gmail.com>
2020-11-23 11:43:02 +0100

    Show a purple GLFW window

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,48 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
+const c = @cImport({
+    @cInclude("epoxy/gl.h");
+    @cInclude("GLFW/glfw3.h");
+});
+
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
+
+fn handleKey(win: ?*c.GLFWwindow, key: c_int, scancode: c_int, action: c_int, mods: c_int) callconv(.C) void {
+    if (action != c.GLFW_PRESS) return;
+    switch (key) {
+        c.GLFW_KEY_ESCAPE => c.glfwSetWindowShouldClose(win, c.GL_TRUE),
+        else => {},
+    }
+}
+
+pub fn main() void {
+    const ok = c.glfwInit();
+    if (ok == 0) {
+        std.debug.panic("Failed to init GLFW\n", .{});
+    }
+    defer c.glfwTerminate();
+
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
+    c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
+    c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+
+    var window = c.glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Learn OpenGL", null, null);
+    if (window == null) {
+        std.debug.panic("Failed to create window\n", .{});
+    }
+    defer c.glfwDestroyWindow(window);
+
+    _ = c.glfwSetKeyCallback(window, handleKey);
+    c.glfwMakeContextCurrent(window);
+
+    while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
+        c.glClearColor(0.2, 0.1, 0.5, 1.0);
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        c.glfwSwapBuffers(window);
+        c.glfwPollEvents();
+    }
 }
