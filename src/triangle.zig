f11d895a85d4adbb56082005f802714fe51f0dd4
Johannes Maier <johb.maier@gmail.com>
2020-11-24 17:08:14 +0100

    Hack a rectangle
    This needs some refactoring. I have to read up on how to abstract in Zig.

--- src/triangle.zig
+++ src/triangle.zig
@@ -28,6 +28,7 @@ pub fn main() void {
     c.glfwWindowHint(c.GLFW_CONTEXT_VERSION_MINOR, 3);
     c.glfwWindowHint(c.GLFW_OPENGL_PROFILE, c.GLFW_OPENGL_CORE_PROFILE);
     c.glfwWindowHint(c.GLFW_OPENGL_FORWARD_COMPAT, c.GL_TRUE);
+    c.glfwWindowHint(c.GLFW_RESIZABLE, c.GL_FALSE);
 
     var window = c.glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Learn OpenGL", null, null);
     if (window == null) {
@@ -37,10 +38,93 @@ pub fn main() void {
 
     _ = c.glfwSetKeyCallback(window, handleKey);
     c.glfwMakeContextCurrent(window);
+    const vertex_shader_source: []const u8 =
+        \\#version 330 core
+        \\
+        \\layout (location = 0) in vec3 aPos;
+        \\
+        \\void main()
+        \\{
+        \\    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        \\}
+    ;
+    const vertex_shader = c.glCreateShader(c.GL_VERTEX_SHADER);
+    c.glShaderSource(vertex_shader, 1, &vertex_shader_source.ptr, null);
+    c.glCompileShader(vertex_shader);
+
+    var success: c_int = undefined;
+    var info_log: [512]u8 = undefined;
+    c.glGetShaderiv(vertex_shader, c.GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+        c.glGetShaderInfoLog(vertex_shader, 512, null, &info_log);
+    }
+
+    const fragment_shader_source: []const u8 =
+        \\#version 330 core
+        \\out vec4 FragColor;
+        \\
+        \\void main()
+        \\{
+        \\    FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        \\}
+    ;
+    const fragment_shader = c.glCreateShader(c.GL_FRAGMENT_SHADER);
+    c.glShaderSource(fragment_shader, 1, &fragment_shader_source.ptr, null);
+    c.glCompileShader(fragment_shader);
+
+    c.glGetShaderiv(fragment_shader, c.GL_COMPILE_STATUS, &success);
+    if (success == 0) {
+        c.glGetShaderInfoLog(fragment_shader, 512, null, &info_log);
+    }
+
+    const shader_program = c.glCreateProgram();
+    c.glAttachShader(shader_program, vertex_shader);
+    c.glAttachShader(shader_program, fragment_shader);
+    c.glLinkProgram(shader_program);
+    // TODO: check info log here
+    c.glDeleteShader(vertex_shader);
+    c.glDeleteShader(fragment_shader);
+
+    const vertices = [_]f32{
+        0.5,  0.5,  0.0,
+        0.5,  -0.5, 0.0,
+        -0.5, -0.5, 0.0,
+        -0.5, 0.5,  0.0,
+    };
+    const indices = [_]u32{
+        0, 1, 3,
+        1, 2, 3,
+    };
+
+    var vao: c_uint = undefined;
+    c.glGenVertexArrays(1, &vao);
+    defer c.glDeleteVertexArrays(1, &vao);
+    var vbo: c_uint = undefined;
+    c.glGenBuffers(1, &vbo);
+    defer c.glDeleteBuffers(1, &vbo);
+    var ebo: c_uint = undefined;
+    c.glGenBuffers(1, &ebo);
+    defer c.glDeleteBuffers(1, &ebo);
+
+    c.glBindVertexArray(vao);
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, vbo);
+    c.glBufferData(c.GL_ARRAY_BUFFER, vertices.len * @sizeOf(f32), &vertices, c.GL_STATIC_DRAW);
+    c.glBindBuffer(c.GL_ELEMENT_ARRAY_BUFFER, ebo);
+    c.glBufferData(c.GL_ELEMENT_ARRAY_BUFFER, indices.len * @sizeOf(u32), &indices, c.GL_STATIC_DRAW);
+
+    c.glVertexAttribPointer(0, 3, c.GL_FLOAT, c.GL_FALSE, 3 * @sizeOf(f32), null);
+    c.glEnableVertexAttribArray(0);
+
+    c.glBindBuffer(c.GL_ARRAY_BUFFER, 0);
+    c.glBindVertexArray(0);
 
     while (c.glfwWindowShouldClose(window) == c.GL_FALSE) {
-        c.glClearColor(0.2, 0.1, 0.5, 1.0);
+        c.glClearColor(0.2, 0.1, 0.2, 1.0);
         c.glClear(c.GL_COLOR_BUFFER_BIT);
+
+        c.glUseProgram(shader_program);
+        c.glBindVertexArray(vao);
+        c.glDrawElements(c.GL_TRIANGLES, 6, c.GL_UNSIGNED_INT, null);
 
         c.glfwSwapBuffers(window);
         c.glfwPollEvents();
